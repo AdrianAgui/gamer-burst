17c41450b757e3e54e15e3832ae2b708a20ad9e6
AdriAgui <adrian.agui9@gmail.com>
2023-01-01 22:32:35 +0100

    eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 export default function App() {
   const [count, setCount] = useState(0)
-  
+
   return (
     <main>
       <h1 className="text-3xl">gamer burst</h1>

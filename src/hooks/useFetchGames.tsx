810cf58d800bb87798f396aace5f106e2ab0c8da
AdriAgui <adrian.agui9@gmail.com>
2023-01-14 21:25:17 +0100

    add random fixed price to games

--- src/hooks/useFetchGames.tsx
+++ src/hooks/useFetchGames.tsx
@@ -1,9 +1,14 @@
 import { Game } from '@/models/game.model'
 import { API, HOME_PAGE_ITEMS } from '@/utils/constants'
+import { decimalHash } from '@/utils/utils'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const HOME_GAMES_URI = `${API.URL}/games?key=${API.KEY}&page_size=${HOME_PAGE_ITEMS}`
+
+const gamesAdapter = (games: Game[]) => {
+  return games.map((game) => ({ ...game, price: decimalHash(game.name) }))
+}
 
 export default function useFetchGames() {
   const [games, setGames] = useState<Game[]>()
@@ -17,7 +22,8 @@ export default function useFetchGames() {
       setIsLoading(true)
       try {
         const response = await axios.get(HOME_GAMES_URI)
-        setGames(response.data.results as Game[])
+        const games = gamesAdapter(response.data.results) as unknown
+        setGames(games as Game[])
         setError(false)
       } catch (err) {
         console.error(err)
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,15 @@ export const getLocalStorage = (key: string) => {
 export const setLocalStorage = (key: string, value: any) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
+
+export const decimalHash = (gameName: string) => {
+  let sum = 0
+  for (let i = 0; i < gameName.length; i++) {
+    const num = gameName.codePointAt(i)
+    if (num) {
+      const op = (i + 1) * num
+      sum += op / (1 << 8)
+    }
+  }
+  return (sum % 60).toFixed(2)
+}

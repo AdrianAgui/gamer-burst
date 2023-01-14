f3783b1af410e499a3876a89c5681f7f220f8833
AdriAgui <adrian.agui9@gmail.com>
2023-01-14 21:24:33 +0100

    remove redux and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import Home from './pages/Home/Home'
 import { CssBaseline } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
-
 import useTheme from './hooks/useTheme'
-import { LayoutContainer } from './styled-components/layout.styled.component'
 
 export default function App() {
   const { colorMode, theme } = useTheme()
@@ -17,13 +13,11 @@ export default function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <CssBaseline />
-          <Navbar />
-          <LayoutContainer>
-            <Home />
-          </LayoutContainer>
-        </Provider>
+        <CssBaseline />
+        <Navbar />
+        <div className='home-container'>
+          <Home />
+        </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
   )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,12 @@
 import useFetchGames from '@/hooks/useFetchGames'
-import { addToCart, removeFromCart } from '@/redux/states/cart'
-import { useDispatch, useSelector } from 'react-redux'
-import { AppStore } from '../../redux/store'
+import { Game } from '@/models/game.model'
 
 export default function Home() {
   const { isLoading, error, games } = useFetchGames()
-  const dispatch = useDispatch()
 
-  const currentCart: string[] = useSelector((store: AppStore) => store.cart)
-
-  const handleClick = (gameName: string) =>
-    currentCart.includes(gameName) ? dispatch(removeFromCart(gameName)) : dispatch(addToCart(gameName))
+  const handleClick = (game: Game) => {
+    console.log(game)
+  }
 
   return (
     <>
@@ -20,11 +16,10 @@ export default function Home() {
         <p>An error occured while fetching users</p>
       ) : (
         <div>
-          {currentCart}
           {games && (
             <ul className='flex flex-wrap justify-center items-center gap-16'>
               {games.map((game) => (
-                <li key={game.slug} onClick={() => handleClick(game?.name)} className='flex flex-col items-center'>
+                <li key={game.slug} onClick={() => handleClick(game)} className='flex flex-col items-center'>
                   <p className='text-xl font-semibold ml-1'>{game.name}</p>
                   <img className='w-96 h-80 object-cover' src={game.background_image} alt={game.slug} loading='lazy' />
                 </li>

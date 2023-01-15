db9031317520039715381d0850e231d91253e751
AdriAgui <adrian.agui9@gmail.com>
2023-01-15 23:32:08 +0100

    fetch game info

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,3 +1,19 @@
+import { useParams } from 'react-router-dom'
+import useFetchGameInfo from '../../hooks/useFetchGameInfo'
+
 export default function Detail() {
-  return <div>DETAIL PAGE</div>
+  const { id } = useParams()
+  const { isLoading, error, gameInfo } = useFetchGameInfo(id ? id : '0')
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Cargando catálogo...</p>
+      ) : error ? (
+        <p>Ha ocurrido un error al cargar el catálogo</p>
+      ) : (
+        <div>{gameInfo && <div>{gameInfo.name}</div>}</div>
+      )}
+    </div>
+  )
 }

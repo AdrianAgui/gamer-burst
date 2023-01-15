321c47cfb1b730523e3357c9d06cd5a0bea21b70
AdriAgui <adrian.agui9@gmail.com>
2023-01-15 23:37:48 +0100

    routes refactor

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import { router } from './routing'
+import { router } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
